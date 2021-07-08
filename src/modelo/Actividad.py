b91d21035c9871317593a96380723db89f4021b4
Alexander <bryano_98@hotmail.com>
2021-07-08 03:08:13 -0500

    v3

--- src/modelo/Actividad.py
+++ src/modelo/Actividad.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import relationship
 
 from .declarative_base import Base
 
+
 class Actividad(Base):
-    __tablename__="actividad"
-    idActividad = Column(Integer,primary_key = True)
-    denominacionActividad = Column ( String )
-    fecha = Column ( Date )
-    equipoTrabajos = Column ( Integer , ForeignKey ( 'equipo.idEquipo' ) )
+    __tablename__ = "actividad"
+    idActividad = Column(Integer, primary_key=True)
+    denominacionActividad = Column(String)
+    fecha = Column(Date)
+    equipoTrabajos = Column(Integer, ForeignKey('equipo.idEquipo'))
