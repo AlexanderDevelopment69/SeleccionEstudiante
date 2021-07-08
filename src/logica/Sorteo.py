1415e0b18f9a10aca0c4caeb4beac2dd5c884ec3
Yojahida-A <45293103@continental.edu.pe>
2021-07-08 08:11:33 -0500

    sorteo

--- src/logica/Sorteo.py
+++ src/logica/Sorteo.py
@@ -1,6 +1,12 @@
 from sqlalchemy.orm import relationship
 
 from src.modelo.Asignatura import Asignatura
+from src.modelo.Actividad import Actividad
+from src.modelo.Estudiante import Estudiante
+from src.modelo.Equipo import Equipo
+
+
+
 from src.modelo.declarative_base import engine, Base, session
 
 
@@ -19,4 +25,37 @@ class Sorteo():
             return True
         else:
             return False
+
+        def agregar_estudiante(self, nombreEstudiante):
+            busqueda = session.query(Estudiante).filter(Estudiante.nombreEstudiante == nombreAsignatura).all()
+
+            if len(busqueda) == 0:
+                estudiante = Estudiante(nombreEstudiante=nombreEstudiante)
+                session.add(estudiante)
+                session.commit()
+                return True
+            else:
+                return False
+
+        def agregar_equipo(self, nombreEquipo):
+            busqueda = session.query(Equipo).filter(Equipo.nombreEquipo == nombreEquipo).all()
+
+            if len(busqueda) == 0:
+                equipo = Equipo(nombreEquipo=nombreEquipo)
+                session.add(equipo)
+                session.commit()
+                return True
+            else:
+                return False
+
+        def agregar_actividad(self, nombreActividad):
+            busqueda = session.query(Actividad).filter(Actividad.nombreActividad == nombreActividad).all()
+
+            if len(busqueda) == 0:
+                actividad = Actividad(nombreActividad=nombreActividad)
+                session.add(actividad)
+                session.commit()
+                return True
+            else:
+                return False
 
